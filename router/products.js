354c3f95d4471f6af702b91931df5fa986c6db61
AvresCode <servas230@gmail.com>
2022-07-20 11:49:32 +0200

    Modified router

--- router/products.js
+++ router/products.js
@@ -1,38 +1,39 @@
 const { Router } = require("express");
 const Product = require("../models").product;
-const Category = require("../models").category
+const Category = require("../models").category;
 
-const router = new Router()
+const router = new Router();
 
 //http :4000/products
 router.get("/", async (req, res, next) => {
-    try {
-      const products = await Product.findAll({raw:true, include:Category});
-      res.send(products);
-    } catch (e) {
-      console.log(e.message);
-      next(e);
-    }
-  });
-
-// specific product with it's category
-//http :4000/products/id
- router.get("/:id", async(req, res, next) => {
-  try{
-
- const productId = req.params.id
-
-const specificProduct = await Product.findByPk(productId, {include:Category})
-
-if (!specificProduct) {
-  res.status(404).send("Product not found");
-} else {
-  res.send(specificProduct);}
- } catch(e){
+  try {
+    const products = await Product.findAll({ include: Category });
+    res.send(products);
+  } catch (e) {
     console.log(e.message);
     next(e);
   }
- })
+});
 
+// specific product with it's category
+//http :4000/products/id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const productId = req.params.id;
 
-module.exports = router
+    const specificProduct = await Product.findByPk(productId, {
+      include: Category,
+    });
+
+    if (!specificProduct) {
+      res.status(404).send("Product not found");
+    } else {
+      res.send(specificProduct);
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+module.exports = router;
