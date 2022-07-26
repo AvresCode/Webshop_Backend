d7fce7a8b5aa70f43c29c6bf538c02205c9dbd9b
AvresCode <servas230@gmail.com>
2022-07-26 16:49:06 +0200

    Added review to the server and product.js

--- router/products.js
+++ router/products.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const Product = require("../models").product;
 const Category = require("../models").category;
+const Review = require("../models").review;
 
 const router = new Router();
 
@@ -29,6 +30,33 @@ router.get("/:id", async (req, res, next) => {
       res.status(404).send("Product not found");
     } else {
       res.send(specificProduct);
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+//post endpoint to create a review
+
+router.post("/:id/review", async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const { rating, text } = req.body;
+    const userId = 1;
+    // const userId = req.body.userId
+    //when you create, productId: productId
+    // userId: 1
+    if (!rating || !text) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newReview = await Review.create({
+        userId,
+        productId,
+        rating,
+        text,
+      });
+      res.json(newReview);
     }
   } catch (e) {
     console.log(e.message);
